bd97c520f4e335c47b5dfc8a03947ca6f15b9663
apeiron242 <apeiron242@gmail.com>
2021-12-29 21:19:18 +0900

    weather-app: added air quality functionality

--- weather-app/README.md
+++ weather-app/README.md
@@ -3,3 +3,5 @@
 This app is an weather app that could detect user's location and display the weather.
 
 You could also search for the weather of a specific city.
+
+Weather data is derived from [OpenWeather](https://openweathermap.org/api), and air quality data is from [aqicn](https://aqicn.org/)
--- weather-app/index.js
+++ weather-app/index.js
@@ -18,6 +18,7 @@ const getLocation = () => {
 
     const weatherData = await data.json();
     displayWeather(weatherData);
+    displayAirQuality(weatherData.name);
   };
 
   if (navigator.geolocation) {
@@ -25,6 +26,80 @@ const getLocation = () => {
   } else {
     alert("Can't access your location");
   }
+};
+
+const displayAirQuality = async (cityName) => {
+  const airQualityHeader = document.querySelector(".air-quality-header");
+  const airQuality = document.querySelector(".air-quality");
+  const levelElem = airQuality.querySelector(".level");
+  const implicationsTitleElem = airQuality.querySelector(".implications-title");
+  const implicationsElem = airQuality.querySelector(".implications");
+  const statementTitleElem = airQuality.querySelector(".statement-title");
+  const statementElem = airQuality.querySelector(".statement");
+
+  const url = `https://api.waqi.info/feed/${cityName}/?token=cb5abaddb2e7478acc2ed564389240f7c094afdd`;
+
+  const data = await fetch(url);
+  const airQualityData = await data.json();
+  const aqi = airQualityData.data.aqi;
+  let airPollutionLevel;
+  let healthImplications;
+  let cautionaryStatement;
+  let bgColor;
+
+  if (aqi <= 50) {
+    airPollutionLevel = "Good";
+    healthImplications =
+      "Air quality is considered satisfactory, and air pollution poses little or no risk";
+    cautionaryStatement = "None";
+    bgColor = "#019966";
+  } else if (aqi <= 100 && aqi >= 51) {
+    airPollutionLevel = "Moderate";
+    healthImplications =
+      "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.";
+    cautionaryStatement =
+      "Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.";
+    bgColor = "#FFDE33";
+    airQuality.style.color = "black";
+  } else if (aqi <= 150 && aqi >= 101) {
+    airPollutionLevel = "Unhealthy for Sensitive Groups";
+    healthImplications =
+      "Members of sensitive groups may experience health effects. The general public is not likely to be affected.";
+    cautionaryStatement =
+      "Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.";
+    bgColor = "#FF9933";
+    airQuality.style.color = "black";
+  } else if (aqi <= 200 && aqi >= 151) {
+    airPollutionLevel = "Unhealthy";
+    healthImplications =
+      "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects";
+    cautionaryStatement =
+      "Active children and adults, and people with respiratory disease, such as asthma, should avoid prolonged outdoor exertion; everyone else, especially children, should limit prolonged outdoor exertion";
+    bgColor = "#D63059";
+  } else if (aqi <= 300 && aqi >= 201) {
+    airPollutionLevel = "Very Unhealthy";
+    healthImplications =
+      "Health warnings of emergency conditions. The entire population is more likely to be affected.";
+    cautionaryStatement =
+      "Active children and adults, and people with respiratory disease, such as asthma, should avoid all outdoor exertion; everyone else, especially children, should limit outdoor exertion.";
+
+    bgColor = "#661E99";
+  } else {
+    airPollutionLevel = "Hazardous";
+    healthImplications =
+      "Health alert: everyone may experience more serious health effects";
+    cautionaryStatement = "Everyone should avoid all outdoor exertion";
+    bgColor = "#7E0E23";
+  }
+
+  airQualityHeader.innerHTML = "Air Quality";
+  airQuality.style.backgroundColor = bgColor;
+
+  levelElem.innerHTML = `${airPollutionLevel} ( ${aqi} )`;
+  implicationsTitleElem.innerHTML = "Health Implications";
+  implicationsElem.innerHTML = `${healthImplications}`;
+  statementTitleElem.innerHTML = "Cautionary Statement (for PM2.5)";
+  statementElem.innerHTML = cautionaryStatement;
 };
 
 const searchCity = async (e) => {
@@ -40,6 +115,7 @@ const searchCity = async (e) => {
 
     const weatherData = await data.json();
     displayWeather(weatherData);
+    displayAirQuality(weatherData.name);
     newCityName.value = "";
   } catch (err) {
     console.log(err);
@@ -48,7 +124,6 @@ const searchCity = async (e) => {
 };
 
 const displayWeather = (weather) => {
-  console.log("hello");
   const displayWeather = document.querySelector(".display-weather");
   const cityName = document.querySelector(".city-name");
   const generalDescription = document.querySelector(".weather-description");
