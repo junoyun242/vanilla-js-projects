706a96dcb572d60a7751bfaa0f67b26ca9420831
apeiron242 <apeiron242@gmail.com>
2021-12-29 21:21:05 +0900

    weather-app: edited readme

--- weather-app/README.md
+++ weather-app/README.md
@@ -4,4 +4,4 @@ This app is an weather app that could detect user's location and display the wea
 
 You could also search for the weather of a specific city.
 
-Weather data is derived from [OpenWeather](https://openweathermap.org/api), and air quality data is from [aqicn](https://aqicn.org/)
+Weather data is derived from [OpenWeather](https://openweathermap.org/api), and air quality data and explanations are from [aqicn](https://aqicn.org/)
--- weather-app/index.js
+++ weather-app/index.js
@@ -28,80 +28,6 @@ const getLocation = () => {
   }
 };
 
-const displayAirQuality = async (cityName) => {
-  const airQualityHeader = document.querySelector(".air-quality-header");
-  const airQuality = document.querySelector(".air-quality");
-  const levelElem = airQuality.querySelector(".level");
-  const implicationsTitleElem = airQuality.querySelector(".implications-title");
-  const implicationsElem = airQuality.querySelector(".implications");
-  const statementTitleElem = airQuality.querySelector(".statement-title");
-  const statementElem = airQuality.querySelector(".statement");
-
-  const url = `https://api.waqi.info/feed/${cityName}/?token=cb5abaddb2e7478acc2ed564389240f7c094afdd`;
-
-  const data = await fetch(url);
-  const airQualityData = await data.json();
-  const aqi = airQualityData.data.aqi;
-  let airPollutionLevel;
-  let healthImplications;
-  let cautionaryStatement;
-  let bgColor;
-
-  if (aqi <= 50) {
-    airPollutionLevel = "Good";
-    healthImplications =
-      "Air quality is considered satisfactory, and air pollution poses little or no risk";
-    cautionaryStatement = "None";
-    bgColor = "#019966";
-  } else if (aqi <= 100 && aqi >= 51) {
-    airPollutionLevel = "Moderate";
-    healthImplications =
-      "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.";
-    cautionaryStatement =
-      "Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.";
-    bgColor = "#FFDE33";
-    airQuality.style.color = "black";
-  } else if (aqi <= 150 && aqi >= 101) {
-    airPollutionLevel = "Unhealthy for Sensitive Groups";
-    healthImplications =
-      "Members of sensitive groups may experience health effects. The general public is not likely to be affected.";
-    cautionaryStatement =
-      "Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.";
-    bgColor = "#FF9933";
-    airQuality.style.color = "black";
-  } else if (aqi <= 200 && aqi >= 151) {
-    airPollutionLevel = "Unhealthy";
-    healthImplications =
-      "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects";
-    cautionaryStatement =
-      "Active children and adults, and people with respiratory disease, such as asthma, should avoid prolonged outdoor exertion; everyone else, especially children, should limit prolonged outdoor exertion";
-    bgColor = "#D63059";
-  } else if (aqi <= 300 && aqi >= 201) {
-    airPollutionLevel = "Very Unhealthy";
-    healthImplications =
-      "Health warnings of emergency conditions. The entire population is more likely to be affected.";
-    cautionaryStatement =
-      "Active children and adults, and people with respiratory disease, such as asthma, should avoid all outdoor exertion; everyone else, especially children, should limit outdoor exertion.";
-
-    bgColor = "#661E99";
-  } else {
-    airPollutionLevel = "Hazardous";
-    healthImplications =
-      "Health alert: everyone may experience more serious health effects";
-    cautionaryStatement = "Everyone should avoid all outdoor exertion";
-    bgColor = "#7E0E23";
-  }
-
-  airQualityHeader.innerHTML = "Air Quality";
-  airQuality.style.backgroundColor = bgColor;
-
-  levelElem.innerHTML = `${airPollutionLevel} ( ${aqi} )`;
-  implicationsTitleElem.innerHTML = "Health Implications";
-  implicationsElem.innerHTML = `${healthImplications}`;
-  statementTitleElem.innerHTML = "Cautionary Statement (for PM2.5)";
-  statementElem.innerHTML = cautionaryStatement;
-};
-
 const searchCity = async (e) => {
   e.preventDefault();
   const newCityName = document.querySelector(".new-city input");
@@ -176,6 +102,80 @@ const displayWeather = (weather) => {
   }
 
   mainSelector.style.backgroundColor = background;
+};
+
+const displayAirQuality = async (cityName) => {
+  const airQualityHeader = document.querySelector(".air-quality-header");
+  const airQuality = document.querySelector(".air-quality");
+  const levelElem = airQuality.querySelector(".level");
+  const implicationsTitleElem = airQuality.querySelector(".implications-title");
+  const implicationsElem = airQuality.querySelector(".implications");
+  const statementTitleElem = airQuality.querySelector(".statement-title");
+  const statementElem = airQuality.querySelector(".statement");
+
+  const url = `https://api.waqi.info/feed/${cityName}/?token=cb5abaddb2e7478acc2ed564389240f7c094afdd`;
+
+  const data = await fetch(url);
+  const airQualityData = await data.json();
+  const aqi = airQualityData.data.aqi;
+  let airPollutionLevel;
+  let healthImplications;
+  let cautionaryStatement;
+  let bgColor;
+
+  if (aqi <= 50) {
+    airPollutionLevel = "Good";
+    healthImplications =
+      "Air quality is considered satisfactory, and air pollution poses little or no risk";
+    cautionaryStatement = "None";
+    bgColor = "#019966";
+  } else if (aqi <= 100 && aqi >= 51) {
+    airPollutionLevel = "Moderate";
+    healthImplications =
+      "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.";
+    cautionaryStatement =
+      "Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.";
+    bgColor = "#FFDE33";
+    airQuality.style.color = "black";
+  } else if (aqi <= 150 && aqi >= 101) {
+    airPollutionLevel = "Unhealthy for Sensitive Groups";
+    healthImplications =
+      "Members of sensitive groups may experience health effects. The general public is not likely to be affected.";
+    cautionaryStatement =
+      "Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.";
+    bgColor = "#FF9933";
+    airQuality.style.color = "black";
+  } else if (aqi <= 200 && aqi >= 151) {
+    airPollutionLevel = "Unhealthy";
+    healthImplications =
+      "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects";
+    cautionaryStatement =
+      "Active children and adults, and people with respiratory disease, such as asthma, should avoid prolonged outdoor exertion; everyone else, especially children, should limit prolonged outdoor exertion";
+    bgColor = "#D63059";
+  } else if (aqi <= 300 && aqi >= 201) {
+    airPollutionLevel = "Very Unhealthy";
+    healthImplications =
+      "Health warnings of emergency conditions. The entire population is more likely to be affected.";
+    cautionaryStatement =
+      "Active children and adults, and people with respiratory disease, such as asthma, should avoid all outdoor exertion; everyone else, especially children, should limit outdoor exertion.";
+
+    bgColor = "#661E99";
+  } else {
+    airPollutionLevel = "Hazardous";
+    healthImplications =
+      "Health alert: everyone may experience more serious health effects";
+    cautionaryStatement = "Everyone should avoid all outdoor exertion";
+    bgColor = "#7E0E23";
+  }
+
+  airQualityHeader.innerHTML = "Air Quality";
+  airQuality.style.backgroundColor = bgColor;
+
+  levelElem.innerHTML = `${airPollutionLevel} ( ${aqi} )`;
+  implicationsTitleElem.innerHTML = "Health Implications";
+  implicationsElem.innerHTML = `${healthImplications}`;
+  statementTitleElem.innerHTML = "Cautionary Statement (for PM2.5)";
+  statementElem.innerHTML = cautionaryStatement;
 };
 
 getLocation();
